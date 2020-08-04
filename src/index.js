793175f1c7d84882fc4d40966a6920bdb2a99ad1
siyabonga <siyabonga.hlengwa@mirumagency.com>
2020-08-04 08:57:50 +0200

    completing covid app

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+// import './index.css';
+import './style.scss'
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App name="Siya" />
   </React.StrictMode>,
   document.getElementById('root')
 );
